6e08b4d792e7b3ecbec4a993ba1a98822c8aac31
ZubairGhani <ghani.zubairmohammad@gmail.com>
2020-02-11 14:26:48 +0100

    tuesday 11,2020

--- loops.js
+++ loops.js
@@ -271,13 +271,269 @@ while(i<=5){
 // Yannick
 
 
+
+
+
+// i = 0 1 2 3 4 5 6 7 8 9 10.. 100
+// p = 0                        20. ..... 40...
+// Yannick
+
+
+/* 
 for (let i = 0; i <= 100; i++) {
-  let isDivisible = i % 20 === 0;
+  let isDivisible = i *20 === 0; 
   if (isDivisible) {
     console.log(i);
   }
+} */
+
+/* for (let i=10; i<=40; i++){
+  let isDivisible= i%10===6;
+  if (isDivisible){
+    console.log(i);
+  }
+}
+ */
+
+//initial condition INIT
+//iterator
+//processing
+//stop condition
+
+// let i=0
+// while(i<=100){
+//   let isDivisible = i %20 === 0;  
+//   if (isDivisible) {
+//     console.log(i);
+//   }
+//   i++
+// }
+
+/* 
+function sumOdds (n) {
+  let sum = 0
+  
+  for (let i = 1; i <= n; i += 2) {
+    sum += i
+  }
+  
+  return sum
+}
+
+console.log(sumOdds(10)) // 25 */
+
+/* let i = 0
+while (i <= 100) {
+  if (i % 20 === 0) {
+    console.log(i);
+  }
+  i++;
+} */
+
+// print the sum of all numbers from 1 to 5
+// 1 2 3 4 5  -> print 15
+/* let sum =0
+for (let i=1; i<=5; i++){
+  sum+= i;
+ 
+  
+}
+console.log(sum);
+
+let print the sum of 45
+123456789=   45 */
+/* 
+let sum =0
+for (let i=1; i<=9; i++){
+  sum+= i;
+  console.log(i,sum)
+}
+console.log(sum)
+ */
+// print all multiples of 20 from 100 to 0
+// i = 100 80 60 40 20 0
+/* for (let i=100; i>=0; i-=20){
+  console.log(i);
+} */
+/* let i =100
+while (i>=20){
+  console.log(i);
+  i-=20;
+} */
+
+
+/*
+*****
+*****
+*****
+*/
+
+// const ROWS = 3;
+// const COLUMNS = 5;
+
+// for (let i = 0; i < ROWS; i++) {
+
+//   // START: print a single row
+//   let row = '';
+//   for (let j = 0; j < COLUMNS; j++) {
+//     row = row + '*';
+//   }
+//   console.log(row);
+//   // END
+
+// }
+
+// const Rows =5;
+// const COLUMNS=5;
+// for (let i=0; i< Rows; i++){
+//   let row ="";
+//   for (let j=0; j< COLUMNS; j++){
+//     row=row + "*";
+//   } 
+//   console.log(row);
+// }
 
 
 
 
+
+// console.log (card, points)
+
+// function printHello() {
+//   let who = "world";
+//   console.log(
+//     function printHello() {
+//       let who = "world";
+//       console.log(`hello ${who}!`)
+//     }
+   
+    
+
+// let a = 2 +;
+// function alert(params) {
+//   console.log(params)
+// }
+
+// switch (a) {
+//   case 4:
+//     alert('Right!');
+//     break;
+
+//   case 3: // (*) grouped two cases
+//   case 5:
+//     alert('Wrong!');
+//     alert("Why don't you take a math class?");
+//     break;
+
+//   default:
+//     alert('The result is strange. Really.');
+// }
+
+// let card= "J"
+// let points 
+// switch(card){
+//   case 'K':
+//     points = 13
+//       break
+//   case "Q":
+//     points =12
+//       break
+//   case "J":
+//     points = 11
+//       break
+//   case "A":
+//     points = 1
+//       break
+
+//       default:
+         
+// }  
+
+// switch (browser) {                                         
+//   case 'Edge':
+//     alert( "You've got the Edge!" );
+//     break;
+
+//   case 'Chrome':
+//   case 'Firefox':
+//   case 'Safari':
+//   case 'Opera':
+//     alert( 'Okay we support these browsers too' );
+//     break;
+
+//   default:
+//     alert( 'We hope that this page looks ok!' );
+// }
+// let browser
+// if(browser == 'Edge') {
+//   alert("You've got the Edge!");
+// } else if (browser == 'Chrome'
+//  || browser == 'Firefox'
+//  || browser == 'Safari'
+//  || browser == 'Opera') {
+//   alert( 'Okay we support these browsers too' );
+// } else {
+//   alert( 'We hope that this page looks ok!' );
+// }
+
+// let a = +prompt('a', '');
+
+// if (a == 0) {
+//   alert( 0 );
+// }
+// if (a == 1) {
+//   alert( 1 );
+// }
+
+// if (a == 2 || a == 3) {
+//   alert( '2,3' );
+// }
+
+// let userName = prompt("give me your money")
+// function showMessage(){
+//     let message = 'Hi, '+userName;
+//     alert(message);
+// }
+
+// showMessage();
+
+
+// function buildRow(columns){
+// let row ='';
+// for (let i=0;i< columns;i++){
+//   row*= '*';
+// }
+// return row;
+
+// }
+// function buildButton(row, columns){
+//   let button='';
+//   for (let i=0;i<row;i++){
+//     button+=buildRow(columns) +'\n';
+//   }
+//   return button;
+// }
+
+let amount="5"
+
+switch(amount){ 
+  case "Dull":
+    damage=1
+    break
+  case "Cool":
+    damage= 5
+    break
+  case "Bravo":
+    damage= 10
+    break
+  case "Awesome":
+    damage= 15
+    break
+  case "Stylish":
+    damage= 20
+    break  
+  default:
+    alert( "not really");
+
+}
 
